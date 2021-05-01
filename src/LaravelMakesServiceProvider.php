283364fd1f043ba2c247a651a34d682a4fd27902
Daniele Montecchi <dev@digitalion.it>
2021-05-01 17:58:32 +0200

    Created custom commands

---
+++ src/LaravelMakesServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Digitalion\LaravelMakes;
+
+use Digitalion\LaravelMakes\Commands\MakeClassCommand;
+use Digitalion\LaravelMakes\Commands\MakeEnumCommand;
+use Digitalion\LaravelMakes\Commands\MakeHelperCommand;
+use Digitalion\LaravelMakes\Commands\MakeScopeCommand;
+use Digitalion\LaravelMakes\Commands\MakeTraitCommand;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMakesServiceProvider extends ServiceProvider
+{
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		if ($this->app->runningInConsole()) {
+			$this->commands([
+				MakeClassCommand::class,
+				MakeEnumCommand::class,
+				MakeHelperCommand::class,
+				MakeScopeCommand::class,
+				MakeTraitCommand::class,
+			]);
+		}
+	}
+}
