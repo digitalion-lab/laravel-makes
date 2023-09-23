be05aa5011e4fd07838fb6dccfca4ae54e5c9d06
Daniele Montecchi <dev@digitalion.it>
2023-09-23 15:45:00 +0200

    [FIX] Make for enum and php >= 8.1

--- src/Commands/MakeEnumCommand.php
+++ src/Commands/MakeEnumCommand.php
@@ -7,45 +7,49 @@ use Illuminate\Console\GeneratorCommand;
 
 class MakeEnumCommand extends GeneratorCommand
 {
-	use MakeCommandTrait;
+    use MakeCommandTrait;
 
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $name = 'make:enum';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $name = 'make:enum';
 
-	/**
-	 * The type of class being generated.
-	 *
-	 * @var string
-	 */
-	protected $type = 'Enum';
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Enum';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Create a new enum class';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new enum class';
 
 
-	/**
-	 * Get the stub file for the generator.
-	 *
-	 * @return string
-	 */
-	protected function getStub()
-	{
-		$php_versions = explode('.', phpversion());
-		$stub_suffix = intval($php_versions[0] . $php_versions[1]) >= 81 ? '_81' : '';
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        $php_versions = explode('.', phpversion());
+        $stub_suffix = (intval($php_versions[0]) >= 8 && intval($php_versions[1]) >= 1) ? '_8x' : '';
 
-		$filename = strtolower($this->type);
-		$stub = app_path("stubs/$filename$stub_suffix.stub");
-		if (!file_exists($stub)) {
-			$stub = __DIR__ . "/../../stubs/$filename.stub";
-		}
-		return $stub;
-	}
+        $filename = strtolower($this->type);
+        $stub = app_path("stubs/$filename$stub_suffix.stub");
+        if (!file_exists($stub)) {
+            $stub = __DIR__ . "/../../stubs/$filename$stub_suffix.stub";
+        }
+        if (!file_exists($stub)) {
+            $stub = __DIR__ . "/../../stubs/$filename.stub";
+        }
+
+        return $stub;
+    }
 }
